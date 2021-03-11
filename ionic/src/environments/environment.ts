b7aed56c917c479470a41ef097d3ed25110c0eff
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-03-11 16:27:33 -0400

    fis: implementando api url

--- ionic/src/environments/environment.ts
+++ ionic/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  api: 'http://api.rutaspanama.dev.cronapis.com/api',
   firebase: {
     apiKey: 'AIzaSyAwpsAm2BlfpUCa20hymcmxrp97tjCIlPs',
     authDomain: 'rutasapp-305809.firebaseapp.com',
