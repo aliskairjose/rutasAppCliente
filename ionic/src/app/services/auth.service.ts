7bfce01909398368e91ea7883ec665ffac7ccc46
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-03-11 16:27:19 -0400

    feat: auth service

---
+++ ionic/src/app/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class AuthService {
+
+  constructor(
+    private _httpService: HttpService
+  ) { }
+
+  /**
+   * @description Inicio de sesión a la app
+   * @param data Objeto { email, password }
+   * @returns 
+   */
+  login( data: any ): Observable<any> {
+    return this._httpService.post( '/login/user', data )
+  }
+
+  register( data: any ): Observable<any> {
+    return this._httpService.post( '/register/user', data );
+  }
+}
