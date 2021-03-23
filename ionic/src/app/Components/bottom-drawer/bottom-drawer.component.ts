19972a026d0dfc033de3394573205b32ae8c8655
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-03-23 16:21:16 -0400

    format code

--- ionic/src/app/Components/bottom-drawer/bottom-drawer.component.ts
+++ ionic/src/app/Components/bottom-drawer/bottom-drawer.component.ts
@@ -6,22 +6,22 @@ import jsQR from 'jsqr';
 import { Router } from '@angular/router';
 import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions/ngx';
 import { NavController } from '@ionic/angular';
-import {Plugins} from "@capacitor/core";
+import { Plugins } from "@capacitor/core";
 
-const {Keyboard} = Plugins;
+const { Keyboard } = Plugins;
 
 // import { DrawerState } from 'ion-bottom-drawer';
-@Component({
+@Component( {
   selector: 'app-bottom-drawer',
   templateUrl: './bottom-drawer.component.html',
-  styleUrls: ['./bottom-drawer.component.scss'],
-})
+  styleUrls: [ './bottom-drawer.component.scss' ],
+} )
 export class BottomDrawerComponent implements AfterViewInit, OnInit {
-  @ViewChild('bottomDrawer', { read: ElementRef }) bottomDrawer: ElementRef;
-  @ViewChild('seat') seat: ElementRef;
-  @ViewChild('editRutas') editRutas: ElementRef;
-  @ViewChild('video', { static: false }) video: ElementRef;
-  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild( 'bottomDrawer', { read: ElementRef } ) bottomDrawer: ElementRef;
+  @ViewChild( 'seat' ) seat: ElementRef;
+  @ViewChild( 'editRutas' ) editRutas: ElementRef;
+  @ViewChild( 'video', { static: false } ) video: ElementRef;
+  @ViewChild( 'canvas', { static: false } ) canvas: ElementRef;
   bottomDrawerElement: any;
   seatElement: any;
   videoElement: any;
@@ -32,7 +32,7 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
 
 
   // tslint:disable-next-line:no-output-rename
-  @Output('openStateChanged') openState: EventEmitter<boolean> = new EventEmitter();
+  @Output( 'openStateChanged' ) openState: EventEmitter<boolean> = new EventEmitter();
   @Output() emitEvent: EventEmitter<any> = new EventEmitter();
   @Input() component = 'Inicio';
 
@@ -62,7 +62,7 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
     private nativePageTransitions: NativePageTransitions,
     public navctl: NavController,
   ) {
-    console.log('bottom drawer');
+    console.log( 'bottom drawer' );
     this.searchList = [
       {
         status: 'completed',
@@ -107,125 +107,121 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
   }
   ngOnInit() {
 
-    window.addEventListener('keyboardWillShow', (e) => {
-      console.log('keyboard will show with height');
+    window.addEventListener( 'keyboardWillShow', ( e ) => {
+      console.log( 'keyboard will show with height' );
       this.dragable = false;
-    this.gesture.enable(false);
-    if (this.isOpen) {
-          this.toggleDrawer();
-        }
-    });
+      this.gesture.enable( false );
+      if ( this.isOpen ) {
+        this.toggleDrawer();
+      }
+    } );
 
-    Keyboard.addListener('keyboardDidHide', () => {
-      console.log('keyboard will hide');
+    Keyboard.addListener( 'keyboardDidHide', () => {
+      console.log( 'keyboard will hide' );
       this.dragable = true;
-    this.gesture.enable(true);
-    // if (!this.isOpen) {
-    //   this.toggleDrawer();
-    // }
-    });
+      this.gesture.enable( true );
+      // if (!this.isOpen) {
+      //   this.toggleDrawer();
+      // }
+    } );
   }
 
-
-  
-
-
-  readJsonData(dataurl) {
-    return new Promise((resolve, reject) => {
-      fetch(dataurl).then(res => res.json()).then(jsonData => {
-        resolve(jsonData);
-      }).catch(err => {
-        resolve({});
-      });
-    });
+  readJsonData( dataurl ) {
+    return new Promise( ( resolve, reject ) => {
+      fetch( dataurl ).then( res => res.json() ).then( jsonData => {
+        resolve( jsonData );
+      } ).catch( err => {
+        resolve( {} );
+      } );
+    } );
   }
 
   async ngAfterViewInit() {
     this.bottomDrawerElement = this.bottomDrawer.nativeElement;
     // if(userService.rutasFlow == 2 )
     // this.videoElement = this.video.nativeElement;
-    console.log('ngafter');
-    this.openHeight = (this.plt.height() / 100) * 60;
+    console.log( 'ngafter' );
+    this.openHeight = ( this.plt.height() / 100 ) * 60;
 
-    this.gesture = await this.gestureCtlr.create({
+    this.gesture = await this.gestureCtlr.create( {
       el: this.bottomDrawerElement,
       gestureName: 'swipe',
       direction: 'y',
       onMove: ev => {
         // tslint:disable-next-line:curly
-        if (ev.deltaY < -this.openHeight) return;
+        if ( ev.deltaY < -this.openHeight ) return;
         // tslint:disable-next-line:curly
-        if (-ev.deltaY < -36.4705810546875) return;
+        if ( -ev.deltaY < -36.4705810546875 ) return;
         // tslint:disable-next-line:curly
-        if (ev.deltaY < 0) return;
+        if ( ev.deltaY < 0 ) return;
         // tslint:disable-next-line:curly
-        if (ev.deltaY > 0) return;
+        if ( ev.deltaY > 0 ) return;
 
         this.bottomDrawerElement.style.transform = `translateY(${ev.deltaY}px)`;
       },
       onEnd: ev => {
-        if (ev.deltaY < -50 ) {
+        if ( ev.deltaY < -50 ) {
           // tslint:disable-next-line:quotemark
           this.bottomDrawerElement.style.transition = ".4s ease-out";
           this.bottomDrawerElement.style.transform = `translateY(${-this.openHeight}px)`;
-          this.openState.emit(true);
+          this.openState.emit( true );
           this.isOpen = true;
         }
-        else if (ev.deltaY > 50 ) {
-          if (this.userService.rutasFlow === 5) {
+        else if ( ev.deltaY > 50 ) {
+          if ( this.userService.rutasFlow === 5 ) {
             this.bottomDrawerElement.style.transform = `translateY(${-this.openHeight / 2.56}px`;
           } else {
             this.bottomDrawerElement.style.transform = ``;
           }
-          this.openState.emit(false);
+          this.openState.emit( false );
           this.isOpen = false;
         }
       },
       onStart: ev => {
-        console.log('mango', !this.isOpen);
+        console.log( 'mango', !this.isOpen );
       }
-    });
-    this.gesture.enable(true);
+    } );
+    this.gesture.enable( true );
   }
 
   toggleDrawer() {
     this.bottomDrawerElement = this.bottomDrawer.nativeElement;
     // this.openState.emit(!this.isOpen);
-    if (!this.isOpen) {
+    if ( !this.isOpen ) {
       this.bottomDrawerElement.style.transition = '.4s ease-out';
       this.bottomDrawerElement.style.transform = `translateY(${-this.openHeight}px`;
-      this.openState.emit(true);
+      this.openState.emit( true );
       this.isOpen = true;
     }
-    else  {
+    else {
       this.bottomDrawerElement.style.transition = '.4s ease-out';
-      if (this.userService.rutasFlow === 5) {
+      if ( this.userService.rutasFlow === 5 ) {
         this.bottomDrawerElement.style.transform = `translateY(${-this.openHeight / 2.56}px`;
       } else {
         this.bottomDrawerElement.style.transform = ``;
       }
 
-      this.openState.emit(false);
+      this.openState.emit( false );
       this.isOpen = false;
     }
   }
 
-  routeHandler(item) {
+  routeHandler( item ) {
     this.selectedItem = item;
     this.userService.rutasData = item;
     this.bottomDrawerElement = this.bottomDrawer.nativeElement;
-    this.openState.emit(false);
-    this.gesture.enable(false);
+    this.openState.emit( false );
+    this.gesture.enable( false );
     this.dragable = false;
     this.bottomDrawerElement.style.transition = '.4s ease-out';
     this.bottomDrawerElement.style.transform = '';
     // this.userService.setInicioRouteIndex(++userService.rutasFlow);
     // this.userService.setClickedSearchList(item);
     ++this.userService.rutasFlow;
-    this.emitEvent.emit({
+    this.emitEvent.emit( {
       type: 'item-selected',
       data: item
-    });
+    } );
     // this.bottomPosition = -69;
   }
 
@@ -240,9 +236,9 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
 
   async scan() {
 
-    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+    if ( this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA ) {
 
-      if (this.loading) {
+      if ( this.loading ) {
         await this.loading.dismiss();
         this.loading = null;
         this.scanActive = true;
@@ -250,7 +246,7 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
 
       this.canvasElement.height = this.videoElement.videoHeight;
       this.canvasElement.width = this.videoElement.videoWidth;
-      this.canvasContext = this.canvasElement.getContext('2d');
+      this.canvasContext = this.canvasElement.getContext( '2d' );
 
       this.canvasContext.drawImage(
         this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height
@@ -259,33 +255,33 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
         0, 0, this.canvasElement.width, this.canvasElement.height
       );
 
-      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+      const code = jsQR( imageData.data, imageData.width, imageData.height, {
         inversionAttempts: 'dontInvert'
-      });
+      } );
 
-      console.log('code', code);
+      console.log( 'code', code );
 
-      if (code?.data) {
+      if ( code?.data ) {
         this.scanActive = false;
         this.scanResult = code.data;
         ++this.userService.rutasFlow;
         this.bottomDrawerElement.style.transition = '.4s ease-out';
         this.bottomDrawerElement.style.transform = '';
-        this.stream.getTracks().forEach(track => track.stop());
-        this.gesture.enable(true);
+        this.stream.getTracks().forEach( track => track.stop() );
+        this.gesture.enable( true );
         this.dragable = true;
-        this.emitEvent.emit({
+        this.emitEvent.emit( {
           type: 'scan-success'
-        });
+        } );
       } else {
-        if (this.scanActive) {
-          requestAnimationFrame(this.scan.bind(this));
+        if ( this.scanActive ) {
+          requestAnimationFrame( this.scan.bind( this ) );
 
         }
       }
 
     } else {
-      requestAnimationFrame(this.scan.bind(this));
+      requestAnimationFrame( this.scan.bind( this ) );
     }
   }
 
@@ -294,35 +290,35 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
     --this.userService.rutasFlow;
     this.bottomDrawerElement.style.transition = '.4s ease-out';
     this.bottomDrawerElement.style.transform = '';
-    this.stream.getTracks().forEach(track => track.stop());
-    this.openState.emit(false);
-    this.gesture.enable(false);
+    this.stream.getTracks().forEach( track => track.stop() );
+    this.openState.emit( false );
+    this.gesture.enable( false );
     this.showScan = false;
     this.scanActive = false;
   }
 
   async scannerOn() {
-    this.loading = await this.loadingCtrl.create({});
+    this.loading = await this.loadingCtrl.create( {} );
     await this.loading.present();
     this.showScan = true;
-    this.stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+    this.stream = await navigator.mediaDevices.getUserMedia( { video: { facingMode: 'environment' } } );
 
     // setTimeout(async () => {
     this.videoElement = this.video.nativeElement;
     this.canvasElement = this.canvas.nativeElement;
     this.videoElement.srcObject = this.stream;
-    this.videoElement.setAttribute('playsinline', true);
+    this.videoElement.setAttribute( 'playsinline', true );
     this.videoElement.play();
 
-    
-    requestAnimationFrame(this.scan.bind(this));
+
+    requestAnimationFrame( this.scan.bind( this ) );
   }
 
   goToFeedback() {
-    this.emitEvent.emit({
+    this.emitEvent.emit( {
       type: 'stop-track'
-    });
-    this.router.navigateByUrl('/sidemenu/Feedback');
+    } );
+    this.router.navigateByUrl( '/sidemenu/Feedback' );
     const options: NativeTransitionOptions = {
       direction: 'left',
       duration: 400,
@@ -332,51 +328,51 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
       // androiddelay: 550,
       // fixedPixelsTop: 0,
       // fixedPixelsBottom: 60
-     };
-    this.nativePageTransitions.slide(options);
-    this.navctl.navigateRoot('/sidemenu/Feedback');
+    };
+    this.nativePageTransitions.slide( options );
+    this.navctl.navigateRoot( '/sidemenu/Feedback' );
     // this.router.navigateByUrl('/sidemenu/Feedback');
   }
 
   editRoute() { // change height after changing this flow
-    this.openHeight = (this.plt.height() / 100) * 60;
+    this.openHeight = ( this.plt.height() / 100 ) * 60;
     // this.bottomPosition = -89;
     this.userService.rutasFlow = 5;
     this.dragable = true;
-    this.openState.emit(true);
-    this.gesture.enable(true);
+    this.openState.emit( true );
+    this.gesture.enable( true );
     this.isOpen = true;
     this.bottomDrawerElement.style.height = '62vh';
     this.bottomDrawerElement.style.transition = '.4s ease-out';
     this.bottomDrawerElement.style.transform = `translateY(${-this.openHeight}px`;
     this.userService.rutasBarEdit = true;
 
-    setTimeout(() => {
-      this.trackScroll(this.editRutas.nativeElement);
-    }, 0);
+    setTimeout( () => {
+      this.trackScroll( this.editRutas.nativeElement );
+    }, 0 );
 
   }
 
   goToSeatArrangement() {
     this.userService.rutasFlow = 4;
-    this.gesture.enable(true);
+    this.gesture.enable( true );
     this.dragable = true;
-    this.readJsonData('../../../assets/jsonCollections/seatMatrix.json').then((jsonData) => {
-      if (jsonData) {
-        console.log('jsonData ==>>>', JSON.parse(JSON.stringify(jsonData)).seats);
-        this.seats = JSON.parse(JSON.stringify(jsonData)).seats;
+    this.readJsonData( '../../../assets/jsonCollections/seatMatrix.json' ).then( ( jsonData ) => {
+      if ( jsonData ) {
+        console.log( 'jsonData ==>>>', JSON.parse( JSON.stringify( jsonData ) ).seats );
+        this.seats = JSON.parse( JSON.stringify( jsonData ) ).seats;
       }
-      this.trackScroll(this.seat.nativeElement);
-    });
+      this.trackScroll( this.seat.nativeElement );
+    } );
   }
 
   goToHome() {
-    this.openHeight = (this.plt.height() / 100) * 60;
+    this.openHeight = ( this.plt.height() / 100 ) * 60;
     this.userService.rutasFlow = 1;
     this.showScan = false;
     this.dragable = false;
-    this.openState.emit(false);
-    this.gesture.enable(false);
+    this.openState.emit( false );
+    this.gesture.enable( false );
     this.bottomDrawerElement.style.transition = '.4s ease-out';
     this.bottomDrawerElement.style.transform = ``;
 
@@ -401,19 +397,19 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
   //   }
   // }
 
-  async trackScroll(ele) {
+  async trackScroll( ele ) {
     this.seatElement = ele;
-    this.seatGesture = await this.gestureCtlr.create({
+    this.seatGesture = await this.gestureCtlr.create( {
       el: this.seatElement,
       gestureName: 'swipe',
       direction: 'y',
       onStart: ev => {
-        this.gesture.enable(false);
+        this.gesture.enable( false );
       },
       onEnd: ev => {
-        this.gesture.enable(true);
+        this.gesture.enable( true );
       }
-    });
-    this.seatGesture.enable(true);
+    } );
+    this.seatGesture.enable( true );
   }
 }
