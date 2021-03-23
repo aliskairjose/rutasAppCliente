e6ee835e3985b673dad6af504ed62aa44912ee03
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-03-23 11:54:13 -0400

    merge

--- ionic/src/app/app.module.ts
+++ ionic/src/app/app.module.ts
@@ -13,44 +13,47 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SidemenuPageModule } from './pages/sidemenu/sidemenu.module';
 
-import {AngularFireModule} from '@angular/fire';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireAuthModule} from '@angular/fire/auth';
-import {UserService} from './services/user.service';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { UserService } from './services/user.service';
 import { FeedbackPageModule } from './pages/feedback/feedback.module';
 import { RatingComponent } from './Components/rating/rating.component';
-import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions/ngx';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
-import {iosTransitionAnimation} from "../../node_modules/@ionic/core/dist/collection/utils/transition/ios.transition.js"
+@NgModule( {
+  declarations: [ AppComponent,
 
-@NgModule({
-  declarations: [AppComponent,
   ],
   entryComponents: [],
   imports: [
     BrowserModule,
     CommonModule,
-    IonicModule.forRoot({mode: 'md'}),
+    IonicModule.forRoot( { mode: 'md' } ),
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     ReactiveFormsModule,
     SidemenuPageModule,
     SoportePageModule,
     InicioPageModule,
     FeedbackPageModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp( environment.firebase ),
     AngularFirestoreModule,
     AngularFireAuthModule,
-    IonicModule.forRoot({  
-      navAnimation: iosTransitionAnimation,
-    }),
+
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NativePageTransitions],
-  bootstrap: [AppComponent],
-  
-})
+  providers: [
+    Geolocation,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
+  ],
+  bootstrap: [ AppComponent ],
+} )
 export class AppModule {
-  constructor(private userService: UserService) {
+  constructor( private userService: UserService ) {
     userService.subscribeBackHandler();
   }
 }
