c6ca2e0c8f6bb547802688982a6d92b63b88201d
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-03-23 12:30:15 -0400

    merge

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -1,26 +1,29 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from './services/storage.service';
 @Component( {
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: [ 'app.component.scss' ],
 } )
-export class AppComponent {
-  public appPages = [
+export class AppComponent implements OnInit {
+  appPages = [
     { title: 'Inicio', url: '/folder/Inicio', icon: 'home' },
     { title: 'Rutas', url: '/folder/Rutas', icon: 'bus' },
     { title: 'Experiencia', url: '/folder/Experiencia', icon: 'medal' },
     { title: 'Soporte/Ayuda', url: '/folder/Soporte', icon: 'help-buoy' },
   ];
-  public labels = [ 'Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders' ];
+  labels = [ 'Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders' ];
+
   constructor(
-    private router: Router
+    private router: Router,
+    private _storage: StorageService,
   ) {
-    const isLoggedin = localStorage.getItem( 'userToken' );
-    if ( isLoggedin ) {
-      this.router.navigate( [ '/sidemenu/Inicio' ] );
-    } else {
-      this.router.navigate( [ '/initial' ] );
-    }
+
+  }
+  async ngOnInit() {
+    const isLoggedin = await this._storage.get( 'rp_token' );
+    const route = isLoggedin ? '/sidemenu/Inicio' : '/initial';
+    this.router.navigate( [ route ] );
   }
 }
