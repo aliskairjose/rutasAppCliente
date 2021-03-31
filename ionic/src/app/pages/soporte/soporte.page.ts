862e2f6f42b9c97ba026a2b643946d0d1b69f02c
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-03-30 21:29:45 -0400

    Notificaciones

--- ionic/src/app/pages/soporte/soporte.page.ts
+++ ionic/src/app/pages/soporte/soporte.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../../services/common.service';
+import { Router } from '@angular/router';
 
 @Component( {
   selector: 'app-soporte',
@@ -10,9 +11,13 @@ export class SoportePage implements OnInit {
 
 
   constructor(
+    private router: Router
   ) { }
 
   ngOnInit() {
   }
 
+  goTo( route: string ): void {
+    this.router.navigate( [ route ] );
+  }
 }
