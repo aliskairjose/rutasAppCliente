3be8bee2167f2c972d1e1355a04201cb1454419b
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-04-06 08:55:49 -0400

    implementando color toast comment

--- ionic/src/app/pages/soporte/support/support.page.ts
+++ ionic/src/app/pages/soporte/support/support.page.ts
@@ -25,8 +25,9 @@ export class SupportPage implements OnInit {
     loading.present();
     this._user.comment( this.comment ).subscribe( () => {
       loading.dismiss();
+      const color = 'primary';
       const message = 'Su comentario ha sido procesado, gracias!';
-      this._common.presentToast( { message } );
+      this._common.presentToast( { message, color } );
     } );
   }
 
