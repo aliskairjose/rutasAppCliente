d07fa0361ae699b2050f78c9b8701f89a3146242
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-04-10 15:01:51 -0400

    mejoramiento de comportamento

--- ionic/src/app/Components/bottom-drawer/bottom-drawer.component.ts
+++ ionic/src/app/Components/bottom-drawer/bottom-drawer.component.ts
@@ -209,7 +209,6 @@ export class BottomDrawerComponent implements AfterViewInit, OnInit {
       const code = jsQR( imageData.data, imageData.width, imageData.height, {
         inversionAttempts: 'dontInvert'
       } );
-      this.userService.rutasFlow = 40;
       if ( code?.data ) {
         this.isOpen = false;
         this.scanActive = true;
