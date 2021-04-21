50e71bd8910650b0835324f4276430cbb469f074
Kervin Gonzalez <kervingonzalez@gmail.com>
2021-04-21 14:02:33 -0400

    condicionando respuesta

--- ionic/src/app/pages/soporte/notification/detail/detail.page.ts
+++ ionic/src/app/pages/soporte/notification/detail/detail.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../../services/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { Comment } from 'src/app/interfaces/comment';
+import { StorageService } from '../../../../services/storage.service';
+import { USER } from '../../../../constants/global-constants';
+import { User } from '../../../../interfaces/user';
 
 @Component( {
   selector: 'app-detail',
@@ -10,15 +13,18 @@ import { Comment } from 'src/app/interfaces/comment';
 } )
 export class DetailPage implements OnInit {
 
+  user: User = {};
   comment: Comment = {};
 
   constructor(
     private route: ActivatedRoute,
+    private _storage: StorageService,
     private userService: UserService,
   ) { }
 
   ngOnInit() {
-    console.log( 'Detail oninit' );
+    this._storage.get( USER ).then( ( user: any ) => this.user = { ...user } );
+
     this.route.queryParams.subscribe( params => {
       this.comment = JSON.parse( window.atob( params.comment ) );
     } );
